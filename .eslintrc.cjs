17e8f539431181fde9f4905ac68a6d7dd69af137
0xLLLLH <linlihao159@gmail.com>
2024-06-04 17:19:56 +0800

    feat: change package org; pin dep version

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 /** @type {import('@types/eslint').Linter.Config} */
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: ['@styled-link/typescript/recommended'],
+  extends: ['@styled.link/typescript/recommended'],
   parserOptions: {
     project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
     tsconfigRootDir: __dirname,
--- packages/eslint-config-lit/src/index.ts
+++ packages/eslint-config-lit/src/index.ts
@@ -1,7 +1,7 @@
 import { type Linter } from 'eslint';
 
 const defaultConfig: Linter.Config = {
-  extends: ['plugin:wc/recommended', 'plugin:lit/recommended', '@styled-link/typescript/recommended'],
+  extends: ['plugin:wc/recommended', 'plugin:lit/recommended', '@styled.link/typescript/recommended'],
 };
 
 export default defaultConfig;
